9d28c403ffd385db473861210c5baf381f89708f
Harry Newman <Harry@live.fi>
2022-03-01 21:16:33 +0000

    Tile Setup
    - Tile prefab made
    - Highlight icons added (configured empty frame and crosshair in the TileInformation script)
    - Material for tile UI added

---
+++ Assets/Scripts/TileInformation.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Script to process and control behaviour of the tile
+ * 
+ * @author H Newman
+ * @version 01-03-2022
+ */
+public class TileInformation : MonoBehaviour
+{
+    // Grid Position Variables
+    public int xPosition;
+    public int zPosition;
+
+    // Is tile shot or not
+    bool shot;
+
+    // To Change sprite highlight based on Tile
+    public SpriteRenderer sprite;
+    // Sprite Array to Store Highlight-Icons
+    public Sprite[] tileHighlights; // 0 = Frame, 1 = Crosshair, 2 = Miss, 3 = Hit
+
+    /** Adds Functionality to Mouse Over Tile to Return Correct Highlight Icon
+	 * 
+	 * @param index - The assosiated number to highlight icon.
+	 */
+    public void HighlightActivate(int index){
+        sprite.sprite = tileHighlights[index];
+    }
+
+    // Reacts when mouse hovers over tile and calls method to show crosshair
+    void OnMouseOver(){
+        HighlightActivate(1);
+    }
+
+    // Only show highlight-icon when needed. Resets to frame.
+    void OnMouseExit() {
+        HighlightActivate(0);
+    }
+    
+
+}
