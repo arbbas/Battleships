31231d6e7da9e4c0365cbee1b469c34812f917a9
Harry Newman <Harry@live.fi>
2022-03-02 20:21:30 +0000

    Placing ghost behaviour added
    - Added behaviour for spaceship of various sizes
    - Added rotation functionality. Can be changed to any angle if we decide to change board tile shape (hexagon)?
    - Test run for tile collision ray to keep track of where spaceships will be placed
    - Added 3D models for Battleships Warships. These are just placeholders until Harsha completes making his own 3D models. 
    - Script to block player placing anything out of bounds or not on their active tile grid

---
+++ Assets/Scripts/GhostActions.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Controls the behaviour of spaceship silhouettes
+ * 
+ * @author Enigma Studios
+ * @version 02-03-2022
+ */
+public class GhostActions : MonoBehaviour
+{
+    //
+
+    public LayerMask layerToCheck;
+
+    // Raycast for hit information
+    RaycastHit hit;
+
+    // Get info from the checked tile
+    TileInformation info;
+
+    // Connection to the playfield
+    PhysicalPlayfield playfield;
+
+    /** Sets the currect playfield in GhostACtions to stop unathorised access to opponent playfield. 
+	 * 
+     * @param _pField - Pass in the current physical playfield.
+	 */
+    public void SetPlayingfield(PhysicalPlayfield _pField)
+    {
+        playfield = _pField;
+    }
+
+    /** Check if over a tile
+	 * 
+     * @return True or False over a tile.
+	 */
+    public bool TileHover()
+    {
+        info = GetTileInformation();
+
+        if (info != null)
+        {
+            // Check if tile is occupied
+            return true;
+        }
+
+        // clears the tile info
+        info = null;
+        return false;
+    }
+
+    /** Sets the currect playfield in GhostACtions to stop unathorised access to opponent playfield. 
+	 * 
+     * @return hit.collider.GetComponent<TileInformation>() - Return info of the tile being checked.
+	 */
+    public TileInformation GetTileInformation()
+    {
+        // Shoots ray down
+        Ray ray = new Ray(transform.position, -transform.up);
+
+        // Physics check on the ray.
+        if (Physics.Raycast(ray, out hit, 1f, layerToCheck))
+        {
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);
+            return hit.collider.GetComponent<TileInformation>();
+        }
+
+        // No tile found
+        return null;
+    }
+}
