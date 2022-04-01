ff9c1c08282173a4ff7af589bcf08b798efde84c
arbbas <94612078+arbbas@users.noreply.github.com>
2022-04-01 19:16:22 +0100

    trying to merge with no conflicts

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-  public void RestartGame()
+    public GameObject optionsScreen;
+    public GameObject Battleshipmainmenu;
+   
+    public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
@@ -19,5 +21,34 @@ public class Menu : MonoBehaviour
     public void LoadGame(string sceneLoad)
     {
         SceneManager.LoadScene(sceneLoad);
+        Battleshipmainmenu.SetActive(false);
+
+    }
+
+    public void OpenOptions()
+    {
+        optionsScreen.SetActive(true);
+        Battleshipmainmenu.SetActive(false);
+        
+    }
+
+    public void CloseOptions()
+    {
+        optionsScreen.SetActive(false);
+        Battleshipmainmenu.SetActive(true);
+
+    }
+
+    public void OpenTutorial()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting application");
+
     }
 }
+
